f2a3752efa0934212d3d6b382d8a9f44b49c647c
Ignacio Alvarez Barrantes <nacho.alvarez2001@gmail.com>
2021-11-22 10:47:16 -0600

    Added Login Message Errors

--- Client/Web-Drive/src/app/material/material.module.ts
+++ Client/Web-Drive/src/app/material/material.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 
 
 
 
 const MaterialComponents = [
-  MatButtonModule,MatInputModule,FormsModule,MatIconModule
+  MatButtonModule,MatInputModule,FormsModule,MatIconModule,ReactiveFormsModule
 ];
 
 @NgModule({
