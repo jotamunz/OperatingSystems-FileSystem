f0ff1157c3c9a32b197926e2f6899bd3984884a7
Jose Pablo Munoz <jpmunoz@estudiantec.cr>
2021-11-22 13:26:17 -0600

    Added get directory content

--- Server/JSONHandler/fileHandler.py
+++ Server/JSONHandler/fileHandler.py
@@ -1,2 +1,18 @@
 from JSONHandler.jsonHandler import *
 
+def getDirContent(path):
+    folders = path.split("/")
+    jsonObject = readJSON(folders[0])
+    if len(folders) == 1:
+        jsonObject.pop("password")
+        jsonObject.pop("size")
+        jsonObject.pop("used")
+    else:
+        folders.pop(0)
+        jsonObject = jsonObject[folders[0]]
+        folders.pop(0)
+        for folder in folders:
+            for directory in jsonObject["directories"]:
+                if directory["name"] == folder:
+                    jsonObject = directory
+    return jsonObject
