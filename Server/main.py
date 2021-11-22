0cd7c24fc0c897d9341d135d3650ca01619ed837
Jose Pablo Munoz <jpmunoz@estudiantec.cr>
2021-11-22 14:02:51 -0600

    Added validation for wrong paths in get contents

--- Server/main.py
+++ Server/main.py
@@ -1,7 +1,7 @@
 from JSONHandler.loginHandler import *
 from JSONHandler.fileHandler import *
 
-print(getDirContent("Soap/root/folder1"))
+print(getDirContent("Soap/root/folder1/subfolder1/t"))
 
 
 
