af6599b2bd8cd24c0bd576dc4744d59db92ed65f
Jose Pablo Munoz <jpmunoz@estudiantec.cr>
2021-11-24 09:33:46 -0600

    Added param for virtual copy

--- Server/main.py
+++ Server/main.py
@@ -1,12 +1,12 @@
 # Imports
-from HTTPServer import app
+#from HTTPServer import app
 from JSONHandler.loginHandler import *
 from JSONHandler.fileHandler import *
 
 # Server start
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000, debug=False)
-    # moveDir("Roo/root", "lmao", "Roo/root/xd")
+    #app.run(host="0.0.0.0", port=8000, debug=False)
+    copyFile("Roo/root/gallito", "file", "Roo/root")
 
 
 
