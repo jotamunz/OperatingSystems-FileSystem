00f6eb8c46131de6d1d19318c22659d13b06c76c
Ignacio Alvarez Barrantes <nacho.alvarez2001@gmail.com>
2021-11-24 00:44:08 -0600

    Added Navigation

--- Client/Web-Drive/src/app/Components/drive/drive.component.ts
+++ Client/Web-Drive/src/app/Components/drive/drive.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../Services/authentication/authentication.service';
+import { DirectoryService } from '../../Services/directory/directory.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import Drive from '../../Models/drive.model';
+import Directory from '../../Models/directory.model';
+import File from '../../Models/file.model';
+import User from '../../Models/user.model';
+
+
 
 @Component({
   selector: 'app-drive',
@@ -6,12 +16,83 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./drive.component.css']
 })
 export class DriveComponent implements OnInit {
-  path = ["Sistemas Operativos","Examenes"];
-  files = [1,2,3,4,5,6];
-  directories = [1,2,3];
-  constructor() { }
 
-  ngOnInit(): void {
+  path: string[] = []; 
+  files: File[] = [];
+  directories : Directory[] = [];
+  directory : Drive = {};
+  user : User = {};
+
+
+  constructor(
+    private routerService: Router,
+    private authenticationService: AuthenticationService,
+    private dirService : DirectoryService,
+    private snackBar: MatSnackBar
+    ) {}
+
+  async ngOnInit(): Promise<void> {
+    this.user = this.authenticationService.getUserInformation();
+    await this.getDir(this.user.username +'/root');
+    this.path.push('root');
   }
 
+
+
+
+/**
+   * Get Current path
+   * @returns current path
+   */
+  public getCurrentPath(){
+    let path = this.user.username + '/';
+    this.path.forEach(segment => {
+      path = path.concat(segment+'/');
+    });
+    return path;
+  }
+
+
+  /**
+   * Change Dir to selected Dir
+   * @returns void
+   */
+  public async onClickChangeDirForward(path:any){
+    this.path.push(path);
+    await this.getDir(this.getCurrentPath()+path);
+    
+  }
+
+   /**
+   * Change Dir to selected Dir
+   * @returns void
+   */
+    public async onClickChangeDirBackward(path:any){
+      let index = this.path.indexOf(path);
+      this.path = this.path.slice(0,index+1);
+      await this.getDir(this.getCurrentPath());
+      
+    }
+
+  /**
+   * Get dir from specific path
+   * @returns void
+   */
+  public async getDir(path:string): Promise<void> {
+    try {
+
+      this.directory =  await this.dirService.getDir(path);
+      this.directories = this.directory.directories as Directory[];
+      this.files = this.directory.files as File[];
+       
+
+    } catch (err: any) {
+      const { message } = err.error;
+      this.snackBar.open(message, 'Close', {
+        verticalPosition: 'top',
+        duration: 3000,
+      });
+    }
+  }
 }
+
--- Client/Web-Drive/src/app/Services/directory/directory.service.ts
+++ Client/Web-Drive/src/app/Services/directory/directory.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import Drive from '../../Models/drive.model';
+import { getLocaleDirection } from '@angular/common';
+import { lastValueFrom } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DirectoryService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+   /**
+   * Retrieves from server directories data
+   * @param dirData The retrieved info
+   * @returns The response from the API
+   */
+
+ public getDir(path: string): Promise<any>{
+  return lastValueFrom(this.httpClient.get('/api/dirs?dirPath='+path));
 }
+}
+
